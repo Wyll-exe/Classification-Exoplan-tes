2896d75296f48d9f4df9e6616481dc5025120dc1
L0wBly <arthurboutry2005@icloud.com>
2025-05-28 14:49:05 +0200

    :bug:

--- exo.py
+++ exo.py
@@ -65,22 +65,22 @@ print("Systèmes doubles : ", df[df['sy_snum'] >= 2].shape[0])
 print("\n-----Question 10-----\n")
 print("Découvertes controversées : ", df[df['pl_controv_flag'] >= 1].shape[0])
 
-# 11 - Y a-t-il des doublons ? Vous pourrez utiliser la fonction duplicated() pour répondre à cette question.
+# 11 - En quelle année a-t-on découvert le plus d’exoplanètes ?
 
-# 12 - Tracer la distribution de chaque indicateur (fonction histplot() de seaborn)
+# 12 - Y a-t-il des doublons ? Vous pourrez utiliser les fonctions duplicated() pour répondre à cette question.	 Vous pouvez explorer la fonction drop_duplicates() de Python ensuite pour vous séparer des doublons.
 
-# 13 - Tracer un boxplot pour chaque indicateur (fonction boxplot() de seaborn)
+# 13 - Tracer la distribution de chaque indicateur (fonction histplot() de seaborn)
 
-# 14 - Calculer la matrice de corrélation et afficher là avec la fonction heatmap() de seaborn. Y a-t-il corrélation entre certaines variables ?
+# 14 - Tracer un boxplot pour chaque indicateur (fonction boxplot() de seaborn)
 
-# 15 - La troisième loi de Kepler est-elle vérifiée dans le dataset ?
+# 15 - Calculer la matrice de corrélation et afficher là avec la fonction heatmap() de seaborn. Y a-t-il corrélation entre certaines variables ?
 
-# 16 - Tracer le graphique suivant :
+# 16 - La troisième loi de Kepler est-elle vérifiée dans le dataset ?
 
-# 17 - Partout où c’est possible, calculez la densité de la planète avec la formule suivante, exprimant le rapport entre la masse de la planète et son volume.
+# 17 - Tracer le graphique suivant :
 
-# 18 - Remplissez une nouvelle colonne “composition” avec les labels “rocky” (rocheux) ou “gaseous” (gazeux) suivant la densité de la planète.
+# 18 - Partout où c’est possible, calculez la densité de la planète avec la formule suivante, exprimant le rapport entre la masse de la planète et son volume :
 
-# 19 - Quel critère utiliser pour remplir les labels sur la composition à partir des planètes du système solaire ? Vous pourrez utiliser un critère de seuil et un algorithme de classification supervisée, entraîné sur les données du système solaire, puis comparer les résultats.
+# 19 - Remplissez une nouvelle colonne “composition” avec les labels “rocky” (rocheux) ou “gaseous” (gazeux) suivant la densité de la planète. Pour information, voici les données des planètes du Système Solaire et leurs labels :
 
-# 20 - En colorisant vos graphiques à l’aide des colonnes “planet_type” et / ou “composition” (argument hue dans le scatterplot de seaborn), tracer des équivalents des graphiques suivant. Attention à l’échelle utilisée !
+# 20 - Quel critère utiliser pour remplir les labels sur la composition à partir des planètes du système solaire ? Vous pourrez utiliser un critère de seuil et un algorithme de classification supervisée, entraîné sur les données du système solaire, puis comparer les résultats.
